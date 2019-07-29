6aeffe4f861be1759f7f187aa60cf24ffbe309c1
Brendan Brown <pbbrown1112@gmail.com>
2019-07-29 15:24:35 -0500

    added wind directions

--- src/components/WeatherCards.js
+++ src/components/WeatherCards.js
@@ -16,8 +16,40 @@ class WeatherCards extends Component {
   getDay = () => {
     this.props.onClick(this.props.day.dt_txt.substring(0,10))
   }
+
+  getWindDirection = () => {
+    let windDirection = this.props.day.wind.deg
+      
+    if ( windDirection > 0 && windDirection < 90 ) {
+        windDirection = "NE"
+    }
+    else if ( windDirection > 90 && windDirection < 180 ) {
+        windDirection = "NW"
+    }
+    else if ( windDirection > 180 && windDirection < 270 ) {
+        windDirection =  "SW"
+    }
+    else if ( windDirection > 270 && windDirection < 360 ) {
+        windDirection =  "SE"
+    }
+    else if ( windDirection === 0) {
+        windDirection =  "E"
+    }
+    else if ( windDirection === 90) {
+        windDirection =  "N"
+    }
+    else if ( windDirection === 180) {
+        windDirection =  "W"
+    }
+    else if ( windDirection === 270) {
+        windDirection =  "S"
+    }
+    return windDirection
+
+  }
   
   render() {
+      console.log(this.props.day.wind.deg)
     // console.log(this.props.day)
     // console.log(typeof this.props.day.main.humidity)
    if(!this.props.day){
@@ -33,7 +65,7 @@ class WeatherCards extends Component {
         <h4>Humidity: {this.props.day.main.humidity}</h4>
         <h4> Temperature: {Math.round((this.props.day.main.temp-273.15)*9/5+32)}</h4>
         <h4>Wind speed: {Math.round(this.props.day.wind.speed*2.237)} mph </h4>
-        <h4>Wind direction: {Math.round(this.props.day.wind.deg)} degrees </h4>
+        <h4>Wind direction: {this.getWindDirection()} </h4>
         {/* {this.props.weather.list.map(w => { 
           return console.log("wtf")
         })} */}
