941b1afc24b7aa2303f500d5aa53cf4d41b9d9bb
github username <github email address>
2019-07-29 15:24:57 -0500

    luis

--- src/components/WeatherCards.js
+++ src/components/WeatherCards.js
@@ -33,7 +33,7 @@ class WeatherCards extends Component {
         <h4>Humidity: {this.props.day.main.humidity}</h4>
         <h4> Temperature: {Math.round((this.props.day.main.temp-273.15)*9/5+32)}</h4>
         <h4>Wind speed: {Math.round(this.props.day.wind.speed*2.237)} mph </h4>
-        <h4>Wind direction: {Math.round(this.props.day.wind.deg)} degrees </h4>
+        <h4>Wind direction: {Math.round(this.props.day.wind.deg)}  </h4>
         {/* {this.props.weather.list.map(w => { 
           return console.log("wtf")
         })} */}
